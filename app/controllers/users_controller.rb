362fcb5eb10b7816c1e62881ca853dc66e43cda5
ooshima manami <maachan.s2.0622@gmail.com>
2019-06-07 18:26:01 +0900

    incremental search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
@@ -12,7 +17,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
+  
   private
 
   def user_params
